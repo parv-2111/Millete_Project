bcc22a86464bdad79bc74ddf6e223941969552df
parv-2111 <parvmehta036@gmail.com>
2026-02-25 21:57:08 +0530

    index

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<?php include("inc/style.php") ?>
+<body>
+
+<!--wrapper start-->
+<div class="wrapper">
+ <main class="main-content">
+
+    <!--== Start Hero Area Wrapper ==-->
+    <section class="home-slider-area">
+      <div class="home-slider-container default-slider-container">
+        <div class="home-slider-wrapper slider-default">
+          <div class="slider-content-area" data-bg-img="assets/img/slider/slider-bg.jpg">
+            <div class="container pt--0 pb--0">
+              <div class="slider-container">
+                <div class="row justify-content-center align-items-center">
+                  <div class="col-12 col-lg-8">
+                    <div class="slider-content">
+                      <h2 class="title"> Welcome To Healthy Millet Festival </h2>
+                      <p class="desc">By Harivanadana College</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--== End Hero Area Wrapper ==-->
+
+    <!--== Start Job Category Area Wrapper ==-->
+    <section class="job-category-area">
+      <div class="container" data-aos="fade-down">
+        <div class="row">
+          <div class="col-12">
+            <div class="section-title text-center">
+              <h3 class="title">Popular Millet Items</h3>
+              <div class="desc">
+                <p>By Cooks From Harivanadan</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row row-gutter-20" >
+          <div class="col-sm-6 col-lg-3">
+            <!--== Start Job Category Item ==-->
+            <div class="job-category-item">
+              <div class="content">
+                <h3 class="title"><a href="job-details.html">Raagi No Rotalo</a></h3>
+              </div>
+              <a class="overlay-link" href="job-details.html"></a>
+            </div>
+            <!--== End Job Category Item ==-->
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <!--== Start Job Category Item ==-->
+            <div class="job-category-item">
+              <div class="content">
+                <h3 class="title"><a href="job-details.html">Raagi No Rotalo</a></h3>
+              </div>
+              <a class="overlay-link" href="job-details.html"></a>
+            </div>
+            <!--== End Job Category Item ==-->
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <!--== Start Job Category Item ==-->
+           <div class="job-category-item">
+              <div class="content">
+                <h3 class="title"><a href="job-details.html">Raagi No Rotalo</a></h3>
+              </div>
+              <a class="overlay-link" href="job-details.html"></a>
+            </div>
+            <!--== End Job Category Item ==-->
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <!--== Start Job Category Item ==-->
+            <div class="job-category-item">
+              <div class="content">
+                <h3 class="title"><a href="job-details.html">Raagi No Rotalo</a></h3>
+              </div>
+              <a class="overlay-link" href="job-details.html"></a>
+            </div>
+            <!--== End Job Category Item ==-->
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            <!--== Start Job Category Item ==-->
+            <div class="job-category-item">
+              <div class="content">
+                <h3 class="title"><a href="job-details.html">Raagi No Rotalo</a></h3>
+              </div>
+              <a class="overlay-link" href="job-details.html"></a>
+            </div>
+            <!--== End Job Category Item ==-->
+          </div>
+          <div class="col-sm-6 col-lg-3">
+            
+    </section>
+    <!--== End Job Category Area Wrapper ==-->
+
+
+    <!--== Start Team Area Wrapper ==-->
+    <section class="team-area">
+      <div class="container" data-aos="fade-down">
+        <div class="row">
+          <div class="col-12">
+            <div class="section-title text-center" >
+              <h3 class="title">Delicious Corner</h3>
+              <div class="desc">
+                <p>Healthy Eating Limits Are Ended Here</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <!--== Start Team Item ==-->
+            <div class="team-item">
+             
+              <div class="content">
+                <h4 class="title"><a href="candidate-details.html">Raagi no rotalo</a></h4>
+                <h5 class="sub-title">By The BCA Cookers</h5>
+                <div class="rating-box">
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                </div>
+                <p class="desc">Ingridients<br>Raagi,Wheat,Turmeric</p>
+                <a class="btn-theme btn-white btn-sm" href="candidate-details.html">Intrested More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <!--== Start Team Item ==-->
+            <div class="team-item">
+             
+              <div class="content">
+                <h4 class="title"><a href="candidate-details.html">Raagi no rotalo</a></h4>
+                <h5 class="sub-title">By The BCA Cookers</h5>
+                <div class="rating-box">
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                  <i class="icofont-star"></i>
+                </div>
+                <p class="desc">Ingridients<br>Raagi,Wheat,Turmeric</p>
+                <a class="btn-theme btn-white btn-sm" href="candidate-details.html">Intrested More</a>
+              </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 col-xl-3">
+            <!--== Start Team Item ==-->
+    </section>
+    <!--== End Team Area Wrapper ==-->
+
+<!--=======================Javascript============================-->
+
+<?php include("inc/script.php") ?>
+
+</body>
+
+</html>
